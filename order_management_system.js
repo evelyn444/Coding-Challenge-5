b14f2b34ccfe3250f5d258edde03426fc0cd8478
Evelyn_ Hernandez <eh22@usf.edu>
2024-09-22 22:03:59 -0400

    Create placeOrder function

--- order_management_system.js
+++ order_management_system.js
@@ -8,3 +8,39 @@ const inventory = [
 
 //Task 2: Create an Orders Array of Order Objects
 const orders = []; //empty array
+
+// Task 3: Create a Function to Place an Order
+//checking for stock of ordered items
+
+function placeOrder(_customerName, itemsOrdered) { 
+        const product = inventory.find (product => product.name === item.name); //finding product in inventory
+         itemsOrdered.forEach(item => {
+
+        if (!product) {
+            return (`Error ${item.name} is not available`); // if not found in inventory
+        }
+
+        else if (product.quantity < item.quantity) {
+            return(`Error ${item.name} is out of stock`) // if out of stock
+        }
+
+        else 
+        {product.quantity -= item.quantity;
+            return product.quantity; // if found, takes the order minus the amount in stock
+        }
+    })
+
+
+// Adding to orders array with pending status
+orders.push({
+    _customerName: _customerName,
+    items: itemsOrdered,
+    status: 'Pending'
+});
+console.log (`Order placed for ${_customerName}`);
+
+// Example
+placeOrder( 'Katie Jones', [{name: 'Latte', quantity: 1}]);
+placeOrder('Sarah Miller', [{ name: 'Americano', quantity: 4}]);
+}
+
